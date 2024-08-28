497b526ed478babd10cf6b9773d26eb7628206c5
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-28 15:08:59 +0330

    commited following

--- main.cpp
+++ main.cpp
@@ -1,126 +1,47 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
-#pragma region structs
-struct Student
+struct studentStruct
 {
+    char name[30];
     int id;
-    string name;
-    float gpa;
+    float grades[20];
+    float averageGrade;
 };
-
-#pragma endregion
-
-#pragma region main methods
-// Write a method which reads items of an array and multiplies them.
-int multiplyNumbers(int arr[], int size)
-{
-    // Init result var
-    int product = 1; // Initialize to 1 for multiplication
-    // Iterate over all the array items
-    for (int i = 0; i < size; i++)
-    {
-        // Multiply items
-        product *= arr[i];
-    }
-
-    cout << "Size of array: " << size << endl;
-    return product;
-}
-
-// Write a method which reads items of an array and sums them.
-int sumsNumbers(int arr[], int size)
-{
-    // Init result var
-    int product = 0; // Initialize to 1 for multiplication
-    // Iterate over all the array items
-    for (int i = 0; i < size; i++)
-    {
-        // sum items
-        product += arr[i];
-    }
-
-    cout << "Size of array: " << size << endl;
-    return product;
-}
-
-// write a program which sorts the array (ascending or descending)
-void bubbleSort(int arr[], int size)
-{
-    int temp = 0;
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
-// write a method which reads the number and returns the inverts of the numbers
-int invertNumbers(int number)
-{
-    int invertedNumber = 0;
-
-    while (number)
-    {
-        int lastDigit = number % 10;
-        invertedNumber = invertedNumber * 10 + lastDigit;
-        number = number / 10;
-    }
-
-    return invertedNumber;
-}
-
-void inputStudent(Student student, int id, string name, float gpa)
-{
-    student.id = id;
-    student.name = name;
-    student.gpa = gpa;
-}
-
-void displayStudent(Student student)
-{
-    cout << "Student ID: " << student.id << endl;
-    cout << "Student Name: " << student.name << endl;
-    cout << "Student GPA: " << student.gpa << endl;
-}
-#pragma endregion
 
 int main()
 {
-#pragma region properties
-    Student student;
-    
-    int number = 12345;
-    int numbers[] = {2, 11, 12, 90};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
+    // read 2*2 martris
+    int firstArr[2][3];
+    int secArr[3][4];
+    int resultOfMul = 0;
 
-    int id = 123;
-    string name = "John Doe";
-    float gpa = 3.75;
-#pragma endregion
+    for (int col = 0; col < 2; col++)
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            cin >> firstArr[col][row];
+        }
+    }
+    for (int col = 0; col < 3; col++)
+    {
+        for (int row = 0; row < 4; row++)
+        {
+            cin >> secArr[col][row];
+        }
+    }
 
-#pragma region method action
-    int mulResult = multiplyNumbers(numbers, size);
-    int sumResult = sumsNumbers(numbers, size);
-    int invertNumbResult = invertNumbers(number);
-    inputStudent(student, id, name, gpa);
-    displayStudent(student);
-#pragma endregion
-
-#pragma region outputs
-    cout << "The mul product of the array elements is: " << mulResult << endl;
-    cout << "The sum product of the array elements is: " << sumResult << endl;
-    cout << "The invert product of the array elements is: " << invertNumbResult << endl;
-#pragma endregion
-
+    // calc the inner mul of it
+    for (int col = 0; col < 2; col++)
+    {
+        for (int row = 0; row < 4; row++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                resultOfMul += resultOfMul[col][row] + (firstArr[col][k] * secArr[k][row]);
+            }
+        }
+    }
     return 0;
 }
