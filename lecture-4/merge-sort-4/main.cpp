b53014c9e49d01f8908539f4942a322fed37bc91
Amir Abaris <thisisamirabaris@gmail.com>
2024-11-26 19:28:10 +0330

    simplify logic

--- lecture-4/merge-sort-4/main.cpp
+++ lecture-4/merge-sort-4/main.cpp
@@ -52,7 +52,7 @@ void mergeSort(float arr[], int left, int right)
     if (left >= right)
         return;
 
-    int mid = left + (right - left) / 2;
+    int mid = (left + right) / 2;
     mergeSort(arr, left, mid);
     mergeSort(arr, mid + 1, right);
     merge(arr, left, mid, right);
