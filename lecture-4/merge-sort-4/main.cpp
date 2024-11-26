7762c82f22516f9272a97a406eda4f208534975b
Amir Abaris <thisisamirabaris@gmail.com>
2024-11-26 19:25:23 +0330

    added merge sort from lec 4

---
+++ lecture-4/merge-sort-4/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+void merge(float arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1; // Size of the left subarray
+    int n2 = right - mid;    // Size of the right subarray
+
+    // Create temporary arrays
+    float* leftArray = new float[n1];
+    float* rightArray = new float[n2];
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++)
+        leftArray[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        rightArray[i] = arr[mid + 1 + i];
+
+    // Merge the temp arrays back into the main array
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy remaining elements of leftArray (if any)
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    // Copy remaining elements of rightArray (if any)
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+
+    // Free up the memory allocated for temp arrays
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+void mergeSort(float arr[], int left, int right)
+{
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+int main() {
+    float arr[] = {38.5, 27.2, 43.1, 3.9, 9.4, 82.1, 10.0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    mergeSort(arr, 0, size - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
